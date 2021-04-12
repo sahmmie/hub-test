791c08fa90b76ad4961220d2cd54aee257c74601
sahmmie <ibrahim.samson@softescu.com>
2021-04-12 05:15:47 +0100

    alomost done

--- src/app/ten12/ten12.component.ts
+++ src/app/ten12/ten12.component.ts
@@ -1,3 +1,4 @@
+import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ten12.component.scss']
 })
 export class Ten12Component implements OnInit {
-
+  fixNav: 'none' | 'blur' | null = null;
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  // adding condition style to navbar on scroll
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const num = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    console.log(num);
+    if (num > 100 && num < 749) {
+      this.fixNav = 'none';
+      console.log(this.fixNav);
+    }
+    else if (num > 750) {
+      this.fixNav = 'blur';
+    }
+    else {
+      this.fixNav = null;
+    }
+  }
 }
